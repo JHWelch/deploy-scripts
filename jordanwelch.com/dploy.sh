5012434c345da0898be082e35724b0e49e490dcb
Jordan Welch <jwelch@highlandsolutions.com>
2021-07-13 20:43:59 -0500

    Add deploy script for jordanwelch.com

---
+++ jordanwelch.com/dploy.sh
@@ -0,0 +1,8 @@
+export PATH="/home/dploy/.nvm/versions/node/v14.16.0/bin/:$PATH"
+cd /var/www/jordanwelch.com/
+echo "Pulling repository."
+git pull
+echo "Installing npm dependencies."
+npm install
+echo "Building npm production."
+npm run prod
