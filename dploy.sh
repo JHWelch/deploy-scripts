3fd96823ab84d5de38151b62f7c8894b9bbc87ff
Jordan Welch <jwelch@highlandsolutions.com>
2021-07-14 10:10:12 -0500

    oath -> path

--- dploy.sh
+++ dploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 AVAILABLE_SITES="choremanager, jordanwelch.com"
 site=$1
-dir_oath="$(dirname $(realpath $0))"
+dir_path="$(dirname $(realpath $0))"
 
 case $site in 
     "choremanager") 
         echo "Deploying ChoreManager"
-        source $dir_oath/ChoreManager/dploy.sh
+        source $dir_path/ChoreManager/dploy.sh
         ;;
     "jordanwelch.com") 
         echo "Deploying jordanwelch.com"
-        source $dir_oath/jordanwelch.com/dploy.sh
+        source $dir_path/jordanwelch.com/dploy.sh
         ;;
     *)
         echo "usage: dploy.sh site"
