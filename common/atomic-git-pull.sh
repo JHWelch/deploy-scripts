67d0e569060198f18dcd3b58594b981de424234f
Jordan Welch <jwelch@highlandsolutions.com>
2021-07-16 17:21:36 -0500

    Use full path

--- common/atomic-git-pull.sh
+++ common/atomic-git-pull.sh
@@ -2,8 +2,7 @@
 base_dir=$1
 repo=$2
 timestamp="$(date +%s)"
-release_dir="./releases/$timestamp"
-
+release_dir="$base_dir/releases/$timestamp"
 cd $base_dir
 git clone $repo $release_dir 2> /dev/null
 ln -s $release_dir ./current
