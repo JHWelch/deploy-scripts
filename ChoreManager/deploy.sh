2e119c7e6a5808d1bca8e495953137ecc0f709b9
Jordan Welch <jwelch@highlandsolutions.com>
2021-07-20 13:59:16 -0500

    Deploy to both production AND demo

--- ChoreManager/deploy.sh
+++ ChoreManager/deploy.sh
@@ -1,47 +1,15 @@
 #!/bin/bash
-export PATH="/home/dploy/.nvm/versions/node/v14.16.0/bin/:$PATH"
-BASE_DIR="/var/www/choremanager"
+PROD_DIR="/var/www/choremanager"
+DEMO_DIR="/var/www/cm-demo"
 
-echo "-------------------------------------"
-echo "Pulling repository into new directory"
-echo "-------------------------------------"
+echo "---------------------------"
+echo "Deploying to Production app"
+echo "---------------------------"
 dir_path="$(dirname $(realpath $0))"
-deploy_path="$(/bin/bash $dir_path/../common/atomic-git-pull.sh $BASE_DIR git@github.com:JHWelch/ChoreManager.git)"
-cd $deploy_path
+/bin/bash $dir_path/../common/deploy-laravel.sh $PROD_DIR git@github.com:JHWelch/ChoreManager.git
 
-echo "-------------------------"
-echo "Link .env"
-echo "-------------------------"
-ln -sf $BASE_DIR/.env .
-
-echo "---------------------------------"
-echo "Installing composer dependencies."
-echo "---------------------------------"
-composer install --no-dev
-
-echo "-------------------------"
-echo "Link storage folder"
-echo "-------------------------"
-rm -rf ./storage
-ln -sfn $BASE_DIR/storage ./storage
-php artisan storage:link
-
-echo "-------------------"
-echo "Migrating database."
-echo "-------------------"
-php artisan migrate --force
-
-echo "-----------------------------"
-echo "Installing yarn dependencies."
-echo "-----------------------------"
-yarn install
-
-echo "-------------------------"
-echo "Building yarn production."
-echo "-------------------------"
-yarn run production
-
-echo "--------------------------------------"
-echo "Linking current folder to new release."
-echo "--------------------------------------"
-/bin/bash $dir_path/../common/atomic-link-current.sh $deploy_path
+echo "---------------------"
+echo "Deploying to Demo app"
+echo "---------------------"
+dir_path="$(dirname $(realpath $0))"
+/bin/bash $dir_path/../common/deploy-laravel.sh $DEMO_DIR git@github.com:JHWelch/ChoreManager.git
