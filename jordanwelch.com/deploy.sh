134114d6758fcda287b938bc33df9b5a19d05853
Jordan Welch <jwelch@highlandsolutions.com>
2021-07-22 16:31:58 -0500

    Format and add gem install

--- jordanwelch.com/deploy.sh
+++ jordanwelch.com/deploy.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 export PATH="/home/dploy/.nvm/versions/node/v14.16.0/bin/:$PATH"
 cd /var/www/jordanwelch.com/
+
+echo "-------------------"
 echo "Pulling repository."
+echo "-------------------"
 git pull
+
+echo "-------------------------------"
+echo "Installing bundle dependencies."
+echo "-------------------------------"
+gem install
+
+echo "----------------------------"
 echo "Installing npm dependencies."
+echo "----------------------------"
 npm install
+
+echo "------------------------"
 echo "Building npm production."
+echo "------------------------"
 npm run prod
