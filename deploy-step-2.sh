d575a4361857490bfa83ee94c1a569a7765665e1
Jordan Welch <jwelch@highlandsolutions.com>
2021-07-22 16:43:47 -0500

    Put the update in its own file so more changes can be automatically deployed.

---
+++ deploy-step-2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+AVAILABLE_SITES="choremanager, jordanwelch.com"
+site=$1
+dir_path="$(dirname $(realpath $0))"
+
+case $site in
+    "choremanager")
+        echo "Deploying ChoreManager"
+        /bin/bash $dir_path/ChoreManager/deploy.sh
+        ;;
+    "jordanwelch.com")
+        echo "Deploying jordanwelch.com"
+        /bin/bash $dir_path/jordanwelch.com/deploy.sh
+        ;;
+    *)
+        echo "usage: deploy.sh site"
+        echo "Available sites: $AVAILABLE_SITES"
+esac
